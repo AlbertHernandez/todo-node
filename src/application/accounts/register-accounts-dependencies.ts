2d7ea4ec7c2971ac16a5e8e3009d09a0d1c2e5c6
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2021-05-22 09:29:25 +0200

    feat: improve register apps with destructuring

--- src/application/accounts/register-accounts-dependencies.ts
+++ src/application/accounts/register-accounts-dependencies.ts
@@ -4,15 +4,18 @@ import { AppDependencies } from '@plugins/interfaces/plugin-interface'
 import { AccountsController, AccountsRepository, AccountsService } from '.'
 import { AccountModel } from './entities'
 
-export const registerAccountsDependencies: AppDependencies = async (app) => {
-  app.logger.trace('Registration of accounts dependencies...')
+export const registerAccountsDependencies: AppDependencies = async ({
+  logger,
+  container
+}) => {
+  logger.trace('Registration of accounts dependencies...')
 
-  app.container.register({
+  container.register({
     accountsController: Awilix.asClass(AccountsController),
     accountsService: Awilix.asClass(AccountsService),
     accountsRepository: Awilix.asClass(AccountsRepository),
     accountModel: Awilix.asValue(AccountModel)
   })
 
-  app.logger.trace('Registration of accounts dependencies completed!')
+  logger.trace('Registration of accounts dependencies completed!')
 }
--- src/application/todos/register-todos-dependencies.ts
+++ src/application/todos/register-todos-dependencies.ts
@@ -4,15 +4,18 @@ import { TodosController, TodosRepository, TodosService } from './'
 import { AppDependencies } from '@plugins/interfaces/plugin-interface'
 import { TodoModel } from './entities/todo-entity'
 
-export const registerTodosDependencies: AppDependencies = async (app) => {
-  app.logger.trace('Registration of todos dependencies...')
+export const registerTodosDependencies: AppDependencies = async ({
+  logger,
+  container
+}) => {
+  logger.trace('Registration of todos dependencies...')
 
-  app.container.register({
+  container.register({
     todosController: Awilix.asClass(TodosController),
     todosService: Awilix.asClass(TodosService),
     todosRepository: Awilix.asClass(TodosRepository),
     todoModel: Awilix.asValue(TodoModel)
   })
 
-  app.logger.trace('Registration of todos dependencies completed!')
+  logger.trace('Registration of todos dependencies completed!')
 }
